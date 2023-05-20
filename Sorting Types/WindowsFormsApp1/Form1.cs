af07b4728a3254b6ee6465f7c5d939a16dcd81d8
Onur Kudret <onurkudrt31@gmail.com>
2023-05-20 20:12:42 +0300

    Sorting Types
    -

---
+++ Sorting Types/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Sort sort = new Sort(); 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string temp="";
+         int[] array = { 1, 5, 7, 8, 14, 90, 44, 11, 56, 74, 3, 76, 106, 4, 0, 22 };
+            for (int i = 0; i < array.Length; i++)
+                temp += array[i].ToString() + " ";
+            MessageBox.Show(temp);
+            temp = "";
+            sort.InsertionSort(array);
+            for (int i = 0; i < array.Length; i++)
+                temp += array[i].ToString() + " ";
+            MessageBox.Show(temp);
+        }
+    }
+}
---
+++ Sorting Types/WindowsFormsApp1/Sort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Sort
+    {
+       public void bubbleSort(int[] array)
+        {
+            int scanned = 0;
+            bool swapped = false;
+            for (scanned=0; scanned<array.Length;scanned++)
+            {
+                swapped = false;
+                for(int i=0; i<(array.Length-scanned-1);i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        swapped = true;
+                        int temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                    }
+                }
+                if (!swapped) break;
+            }
+        }
+        public void SelectionSort(int[] array)
+        {
+            int minIndex;
+            for (minIndex = 0; minIndex<array.Length;minIndex++ )
+            {
+                int newMinIndex = minIndex;
+        
+                for (int i = minIndex + 1; i < array.Length; i++)
+                    if (array[newMinIndex] > array[i])
+                        newMinIndex = i;
+
+                int temp = array[minIndex];
+               array[minIndex] = array[newMinIndex];
+                array[newMinIndex] = temp;
+            }
+        }
+        public void InsertionSort(int[] array)
+        {
+            int scanned = 0;
+            int moved;
+            for(int i=1;i<array.Length;i++)
+            {
+                moved = array[i];
+                for(int j=scanned;j>=0;j--)
+                if(array[j]>moved)
+                    {
+                        int temp = array[j];
+                        array[j] = moved;
+                        array[j + 1] = temp;
+                        
+                    }
+                scanned++;
+            }
+        }
+    }
+}
